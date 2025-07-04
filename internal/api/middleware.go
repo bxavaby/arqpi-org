d54ea79cb1566efaf91e2463cd8b50627eaba9bd
bxavaby <bfernandes4@pm.me>
2025-07-04 22:43:18 +0200

    Add CORS headers to middleware and handle OPTIONS requests correctly

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -26,6 +26,21 @@ func (a *API) RateLimiter(limit int, windowSecs int) func(http.Handler) http.Han
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			origin := r.Header.Get("Origin")
+			if origin == "https://arqpi.org" || origin == "https://www.arqpi.org" || origin == "https://bxavaby.github.io" {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Kofi-Verification-Token")
+				w.Header().Set("Access-Control-Max-Age", "300")
+			}
+
+			// return immediately
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
 			ip := r.RemoteAddr
 
 			apiKey := r.URL.Query().Get("key")
